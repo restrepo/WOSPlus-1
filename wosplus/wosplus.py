0244b53791ff126b55d88d2e0d6f65f47f0524c2
Julián Calle <jcallem94@gmail.com>
2018-03-03 15:49:36 -0500

    Actual

--- wosplus/wosplus.py
+++ wosplus/wosplus.py
@@ -79,7 +79,7 @@ class wosplus:
     The type mus be declared with the 'load_biblio' with the 'prefix' option
     (Default type is WOS)
     """
-    import configparser.ConfigParser as cfg
+    from configparser import ConfigParser
     import re
 
     def __init__(self,cfg_file):
@@ -88,6 +88,7 @@ class wosplus:
         Based on:
         http://stackoverflow.com/a/39225272
         '''
+        cfg=ConfigParser()
         cfg.optionxform=str
         tmp=cfg.read(cfg_file)
         self.drive_file=cfg['FILES']
