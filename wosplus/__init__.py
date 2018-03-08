237901b43bd6fec097ff1a4ec152ee169b9d8d82
Julián Calle <jcallem94@gmail.com>
2018-03-07 23:03:04 -0500

    dist

--- wosplus/__init__.py
+++ wosplus/__init__.py
@@ -11,9 +11,9 @@ import Levenshtein as lv
 from unidecode import unidecode
 from configparser import ConfigParser
 
-from . import wosplus
-from . import wos_scp
-from . import merge_tools
-from . import wos_parser
-from . import google_drive_tools
-from . import pajek_tools
+from . import _wos_scp
+from . import _merge_tools
+from . import _wos_parser
+from . import _google_drive_tools
+from . import _pajek_tools
+from . import _wosplus
