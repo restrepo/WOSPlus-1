2ff4e4468eff01532a47d6263c3e5b768872d5cc
Julián Calle <jcallem94@gmail.com>
2018-03-08 17:29:41 -0500

    Version 0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+For WOSPlus execution it is necessary to define drive.cfg file in which you specify the name of the file and the location of it. The drive.cfg file must be present in the execution folder.
+For example, the drive.cfg file in the test folder gives the addresses of the databases stored in google drive.
+
 <!-- mv diagram to http://interactive.blockdiag.com and links as in https://github.com/jupyter/docker-stacks -->
 # WOS+
 ## Visual Overview
--- setup.py
+++ setup.py
@@ -28,40 +28,44 @@ if os.name in ('nt', 'dos'):
     warning = "WARNING: Windows is not officially supported"
     print(warning, file=sys.stderr)
 
-setup(
-    # Application name:
-    name="WOSplus",
+def main():
+    setup(
+        # Application name:
+        name="WOS+",
 
-    # Version number (initial):
-    version="0.1.0",
+        # Version number (initial):
+        version="0.1.0",
 
-    # Application author details:
-    author="Diego Restrepo",
-    author_email="restrepo@udea.edu.co",
+        # Application author details:
+        author="Diego Restrepo",
+        author_email="restrepo@udea.edu.co",
 
-    # Packages
-    packages=find_packages(exclude=['tests']),
+        # Packages
+        packages=find_packages(exclude=['tests']),
 
-    # Include additional files into the package
-    include_package_data=True,
+        # Include additional files into the package
+        include_package_data=True,
 
-    # Details
-    url="https://github.com/restrepo/WOSplus",
+        # Details
+        url="https://github.com/restrepo/WOSplus",
 
-    #
-    # license="LICENSE.txt",
-    description="WOS+",
+        #
+        license="BSD",
+        description="WOS+ is a tools for to manage and merge database of Web Of Science",
 
-    # long_description=open("README.txt").read(),
+        # long_description=open("README.txt").read(),
 
-    # Dependent packages (distributions)
-    install_requires=[
-        'configparser',
-        'xlrd',
-        'requests',
-        'numpy',
-        'pandas',
-        'unidecode',
-        'python-levenshtein',
-    ],
-)
+        # Dependent packages (distributions)
+        install_requires=[
+            'configparser',
+            'xlrd',
+            'requests',
+            'numpy',
+            'pandas',
+            'unidecode',
+            'python-levenshtein',
+        ],
+    )
+
+if __name__ == "__main__":
+    main()    
